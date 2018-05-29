e13b791aae0b467e46e95fcf139248e12d8ff6eb
mullak99 <mullak99@mullak99.co.uk>
2018-05-29 03:15:49 +0100

    Add project files.

---
+++ CoreChecksums/CoreChecksums.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+
+public class Checksums
+{
+    public string convertHashToString(byte[] hash)
+    {
+        return BitConverter.ToString(hash).Replace("-", "");
+    }
+
+    public bool compareHash(byte[] firstHash, byte[] secondHash)
+    {
+        if (convertHashToString(firstHash) == convertHashToString(secondHash)) return true;
+        else return false;
+    }
+
+    public byte[] getMD5(string inputFile)
+    {
+        using (var md5 = MD5.Create())
+        using (var stream = File.OpenRead(inputFile))
+            return md5.ComputeHash(stream);
+    }
+
+    public byte[] getSHA1(string inputFile)
+    {
+        using (var sha1 = SHA1.Create())
+        using (var stream = File.OpenRead(inputFile))
+            return sha1.ComputeHash(stream);
+    }
+
+    public byte[] getSHA256(string inputFile)
+    {
+        using (var sha256 = SHA256.Create())
+        using (var stream = File.OpenRead(inputFile))
+            return sha256.ComputeHash(stream);
+    }
+
+    public byte[] getSHA512(string inputFile)
+    {
+        using (var sha512 = SHA512.Create())
+        using (var stream = File.OpenRead(inputFile))
+            return sha512.ComputeHash(stream);
+    }
+}
