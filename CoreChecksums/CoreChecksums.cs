1c374b306191f950a43e1781cb2c37882f6139e5
mullak99 <mullak99@mullak99.co.uk>
2018-09-10 09:02:53 +0100

    Builds for both .NET Core 2.0 and .NET Framework 4.5

--- CoreChecksums/CoreChecksums.cs
+++ CoreChecksums/CoreChecksums.cs
@@ -2,44 +2,47 @@
 using System.IO;
 using System.Security.Cryptography;
 
-public class Checksums
+namespace CoreChecksums
 {
-    public string convertHashToString(byte[] hash)
+    public class Checksums
     {
-        return BitConverter.ToString(hash).Replace("-", "");
-    }
+        public static string convertHashToString(byte[] hash)
+        {
+            return BitConverter.ToString(hash).Replace("-", "");
+        }
 
-    public bool compareHash(byte[] firstHash, byte[] secondHash)
-    {
-        if (convertHashToString(firstHash) == convertHashToString(secondHash)) return true;
-        else return false;
-    }
+        public static bool compareHash(byte[] firstHash, byte[] secondHash)
+        {
+            if (convertHashToString(firstHash) == convertHashToString(secondHash)) return true;
+            else return false;
+        }
 
-    public byte[] getMD5(string inputFile)
-    {
-        using (var md5 = MD5.Create())
-        using (var stream = File.OpenRead(inputFile))
-            return md5.ComputeHash(stream);
-    }
+        public static byte[] getMD5(string inputFile)
+        {
+            using (var md5 = MD5.Create())
+            using (var stream = File.OpenRead(inputFile))
+                return md5.ComputeHash(stream);
+        }
 
-    public byte[] getSHA1(string inputFile)
-    {
-        using (var sha1 = SHA1.Create())
-        using (var stream = File.OpenRead(inputFile))
-            return sha1.ComputeHash(stream);
-    }
+        public static byte[] getSHA1(string inputFile)
+        {
+            using (var sha1 = SHA1.Create())
+            using (var stream = File.OpenRead(inputFile))
+                return sha1.ComputeHash(stream);
+        }
 
-    public byte[] getSHA256(string inputFile)
-    {
-        using (var sha256 = SHA256.Create())
-        using (var stream = File.OpenRead(inputFile))
-            return sha256.ComputeHash(stream);
-    }
+        public static byte[] getSHA256(string inputFile)
+        {
+            using (var sha256 = SHA256.Create())
+            using (var stream = File.OpenRead(inputFile))
+                return sha256.ComputeHash(stream);
+        }
 
-    public byte[] getSHA512(string inputFile)
-    {
-        using (var sha512 = SHA512.Create())
-        using (var stream = File.OpenRead(inputFile))
-            return sha512.ComputeHash(stream);
+        public static byte[] getSHA512(string inputFile)
+        {
+            using (var sha512 = SHA512.Create())
+            using (var stream = File.OpenRead(inputFile))
+                return sha512.ComputeHash(stream);
+        }
     }
 }
